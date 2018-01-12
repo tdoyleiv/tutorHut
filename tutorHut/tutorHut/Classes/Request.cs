7d121a04f0c486b71a5bb65138aa9cd1cb16c82e
Tom Doyle <tfdoyleiv@gmail.com>
2018-01-12 09:38:44 -0600

    Added key annotation to request

--- tutorHut/tutorHut/Classes/Request.cs
+++ tutorHut/tutorHut/Classes/Request.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace tutorHut.Classes
     {
         private Subject subject = new Subject();
         private Address location = new Address();
+        [Key]
         public int ID { get; set; }
         public DateTime Time { get; set; }
         public Subject Subject { get { return subject; } set { subject = value; } }
