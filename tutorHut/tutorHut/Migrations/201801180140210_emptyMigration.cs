020b111ec3abedc0a93ee0fb2406f40dd650f777
LanceYang <lanceYang15@gmail.com>
2018-01-17 19:40:52 -0600

    added empty migration

---
+++ tutorHut/tutorHut/Migrations/201801180140210_emptyMigration.cs
@@ -0,0 +1,16 @@
+namespace tutorHut.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class emptyMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
